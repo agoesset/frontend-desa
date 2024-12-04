b20597432e60267917b4123c49d6a15925a83601
agoesset <agussetiawan160802@gmail.com>
2024-12-04 19:16:04 +0700

    add pwa

--- public/web/js/pwa.js
+++ public/web/js/pwa.js
@@ -1,12 +1,37 @@
-// Service Worker Register 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(registration => {
-        //console.log('Service Worker is registered', registration);
+// Service Worker Register
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker is registered", registration);
       })
-      .catch(err => {
-        console.error('Registration failed:', err);
+      .catch((err) => {
+        console.error("Registration failed:", err);
       });
   });
 }
+
+// Add to Home Screen
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+if (addBtn) addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  if (addBtn) addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", (e) => {
+    if (addBtn) addBtn.style.display = "none";
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
--- public/web/service-worker.js
+++ public/web/service-worker.js
@@ -1,54 +1,66 @@
-// -----------------------------------------------------------------
-// Template Name: Suha - Multipurpose Ecommerce Mobile HTML Template
-// Template Author: Designing World
-// Template Author URL: https://themeforest.net/user/designing-world
-// -----------------------------------------------------------------
-
-const staticCacheName = 'precache-v3.2.0';
-const dynamicCacheName = 'runtimecache-v3.2.0';
+const staticCacheName = "precache-v3.2.0";
+const dynamicCacheName = "runtimecache-v3.2.0";
 
 // Pre Caching Assets
 const precacheAssets = [
-    '/',
-    'img/bg-img/no-internet.png',
-    'js/theme-switching.js',
-    'offline.html'
+  "/",
+  "/offline",
+  "/img/icons/icon-72x72.png",
+  "/img/icons/icon-96x96.png",
+  "/img/icons/icon-128x128.png",
+  "/img/icons/icon-144x144.png",
+  "/img/icons/icon-152x152.png",
+  "/img/icons/icon-192x192.png",
+  "/img/icons/icon-384x384.png",
+  "/img/icons/icon-512x512.png",
+  "/img/bg-img/no-internet.png",
+  "/js/theme-switching.js",
+  "/manifest.json",
+  "/web/js/pwa.js",
 ];
 
 // Install Event
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(precacheAssets);
-        })
-    );
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(precacheAssets);
+    })
+  );
 });
 
 // Activate Event
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-                .map(key => caches.delete(key))
-            );
-        })
-    );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 // Fetch Event
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request).then(response => {
-                return caches.open(dynamicCacheName).then(function (cache) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                })
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(event.request).then((response) => {
+            return caches.open(dynamicCacheName).then(function (cache) {
+              // Store the response in cache
+              cache.put(event.request, response.clone());
+              return response;
             });
-        }).catch(function() {
-            // Fallback Page, When No Internet Connection
-            return caches.match('offline.html');
           })
-    );
+        );
+      })
+      .catch(function () {
+        // Fallback Page, When No Internet Connection
+        return caches.match("/offline");
+      })
+  );
 });
